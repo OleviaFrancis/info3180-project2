2b1f19bb6b669bca961f6afef92c6359deea76b2
Olevia Francis <olevia.francis@mymona.uwi.edu>
2019-04-27 03:04:10 +0000

    Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_wtf.csrf import CSRFProtect
 app = Flask(__name__)
 csrf = CSRFProtect(app)
 app.config['SECRET_KEY'] = "thisisasecret"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project2:password123@localhost/project2"   #this is what will give acess to the database
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://eimnxofbvorepj:952d68e415cb3b7b99fa2daa4cf8f53baa21b487f1f3f9136591f11de3fdd857@ec2-54-197-234-117.compute-1.amazonaws.com:5432/d600bhkqukpv3e"   #this is what will give acess to the database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True   #added just to suppress a warning 
 app.config['UPLOAD_FOLDER']="./app/static/uploads"   #profile pictures are stored here
 app.config['PHOTOS']="./app/static/photos"     #posted images are stored here
